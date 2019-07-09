7c36d6e339aed4f2ef888e3d582678364081717e
joemrk <42858950+joemrk@users.noreply.github.com>
2019-07-09 22:28:36 +0300

    ended about

--- js/control.js
+++ js/control.js
@@ -2,5 +2,10 @@
 $(function () {
     $('#info').on('click', function () {
         $('#info-content').toggleClass('info-open');
-    });    
+    });  
+    
+    $('.progress-bar').each(function(){
+        let width = $(this).data('skill')
+        $(this).css('width', width + '%')
+    });
 });
